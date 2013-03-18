c5c3583d835bdb8a48808f4b3481a46c251abc4b
GRuslan <garagonuch@mail.ru>
2013-03-18 17:36:01 +0200

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MebliStil::Application.config.secret_token = '02c0a5d8670d3ae47a152c3eb22c1567b84bb38dfcac07e17328c97f29e58cff4f32b5df0cbb799835538846caad10e46fbd1ff4bc72c0b3dd2b4653bda1def6'
